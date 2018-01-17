4f68f26ee168ac0f88de009f9558a71772c40a24
DmitriyCODER <dmitriyvs85@mail.ru>
2018-01-17 11:29:07 +0700

    * тестируем классы уток

--- main.pas
+++ main.pas
@@ -4,12 +4,16 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs;
+  Dialogs, uDuck, StdCtrls, uFly;
 
 type
   TForm1 = class(TForm)
+    btn1: TButton;
+    procedure btn1Click(Sender: TObject);
   private
     { Private declarations }
+    FMallard:TDuck;
+    FModel:TDuck;
   public
     { Public declarations }
   end;
@@ -20,5 +24,17 @@ var
 implementation
 
 {$R *.dfm}
+
+procedure TForm1.btn1Click(Sender: TObject);
+begin
+  FMallard:=TMallardDuck.Create;
+  FMallard.PerformQuack;
+  FMallard.PerformFly;
+
+  FModel:=TModelDuck.Create;
+  FModel.PerformFly;
+  FModel.SetFlyBehavior(TFlyRocketPowered.Create);
+  FModel.PerformFly;
+end;
 
 end.
