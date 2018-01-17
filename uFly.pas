8bf97c17af04d8b6165ebf2a7f69ab33a464771c
DmitriyCODER <dmitriyvs85@mail.ru>
2018-01-17 11:28:09 +0700

    * добавлен класс поведения на реактивной тяге :)

--- uFly.pas
+++ uFly.pas
@@ -20,6 +20,11 @@ type
     procedure Fly;
   end;
 
+  TFlyRocketPowered=class(TInterfacedObject, IFlyBehavior)
+  public
+    procedure Fly;
+  end;
+
 implementation
 
 { TFlyWithWings }
@@ -35,6 +40,13 @@ end;
 procedure TFlyNoWay.Fly;
 begin
   MessageBox(0,'I can`t fly', 'FlyNoWay!', MB_OK);
+end;
+
+{ TFlyRocketPowered }
+
+procedure TFlyRocketPowered.Fly;
+begin
+  MessageBox(0,'I`m flying with a rocket!', 'FlyRocketPowered!', MB_OK);
 end;
 
 end.
